ca270cdb65e051e1866622e0f49a489b6a550b19
cedunia <cedric.hassenkhodja@gmail.com>
2016-06-14 19:25:09 +0200

    made statistics scripts

---
+++ scripts/rcourse_lesson2_statistics.R
@@ -0,0 +1,26 @@
+## READ IN DATA ##
+source('./scripts/rcourse_lesson2_cleaning.R')
+
+## LOAD PACKAGES ##
+
+
+## ORGANIZE DATA ##
+data_stats <- data_clean
+
+## BUILD MODEL - PROPORTION OF CEDRIC'S BY YEAR (CONTINUOUS PREDICTOR) ##
+year.lm = lm(prop_log10 ~ year, data = data_stats)
+
+year.lm_sum = summary(year.lm)
+year.lm_sum
+
+year.lm_resid = resid(year.lm)
+head(year.lm_resid)
+
+## BUILD MODEL - PROPORTION OF CEDRIC'S BY SEX (CATEGORICAL PREDICTOR) ##
+sex.lm = lm(prop_log10 ~ sex, data = data_stats)
+
+sex.lm_sum = summary(sex.lm)
+sex.lm_sum
+
+sex.lm_resid = resid(sex.lm)
+head(sex.lm_resid)
