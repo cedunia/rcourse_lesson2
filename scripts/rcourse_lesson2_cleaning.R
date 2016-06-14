d6d139a2a00299c5c5462f241653bde3d7959873
cedunia <cedric.hassenkhodja@gmail.com>
2016-06-14 18:19:53 +0200

    made cleaning script

---
+++ scripts/rcourse_lesson2_cleaning.R
@@ -0,0 +1,19 @@
+## LOAD PACKAGES ##
+library(dplyr)
+
+## READ IN DATA ##
+data <- read.table('./data/rcourse_lesson2_data.txt', sep = "\t", 
+                   header = TRUE)
+
+## CLEAN DATA ##
+data_clean <- data %>% filter(., name == 'Cedric')
+xtabs(~name, data_clean)
+data_clean <- data %>% filter(., name == 'Cedric') %>% 
+  mutate(name = factor(name)) %>%
+  filter(year > 1900) %>%
+  filter(year <= 2000)
+
+min(data_clean$year)
+max(data_clean$year)
+
+
